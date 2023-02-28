546e34c83b3bead9b0ff8046619de78da5b58a3b
pancake <guoyt98@163.com>
2023-02-28 17:08:28 +0800

    getgetURLsFromHTML test

--- crawl.js
+++ crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require("jsdom");
+
+function getURLsFromHTML(htmlBody, baseURL) {
+  const urls = [];
+  const dom = new JSDOM(htmlBody);
+  const linkElements = dom.window.document.querySelectorAll("a");
+  for (const linkElement of linkElements) {
+    if (linkElement.href.slice(0, 1) === "/") {
+      //relative url
+      try {
+        const urlObj = new URL(`${baseURL}${linkElement.href}`);
+        urls.push(urlObj.href);
+      } catch (error) {
+        console.log(`error with relative url: ${error.message}`);
+      }
+    } else {
+      //absolute
+      try {
+        const urlObj = new URL(linkElement.href);
+        urls.push(urlObj);
+      } catch (error) {
+        console.log(`error with absolute url: ${error.message}`);
+      }
+    }
+  }
+  return urls;
+}
+
 function normalizeURL(urlString) {
   const urlObj = new URL(urlString);
   const hostPath = `${urlObj.host}${urlObj.pathname}`;
@@ -9,4 +37,5 @@ function normalizeURL(urlString) {
 
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
--- crawl.test.js
+++ crawl.test.js
@@ -1,9 +1,22 @@
-const { normalizeURL } = require("./crawl");
+const { normalizeURL, getURLsFromHTML } = require("./crawl");
 const { test, expect } = require("@jest/globals");
 
 test("normalizeURL strip protocal && slash", () => {
   const input = "https://blog.boot.dev/path/";
   const actual = normalizeURL(input);
-  const expexted = "blog.boot.dev/path";
-  expect(actual).toEqual(expexted);
+  const expected = "blog.boot.dev/path";
+  expect(actual).toEqual(expected);
+});
+
+test("getURLsFromHTML relativeURLs", () => {
+  const inputHTMLBody = `
+  <html>
+    <body>
+      <a href="invalid">invalid BLog</a>
+    </body>
+  </html>`;
+  const inputBaseURL = "https://blog.boot.dev";
+  const actual = getURLsFromHTML(inputHTMLBody, inputBaseURL);
+  const expected = [];
+  expect(actual).toEqual(expected);
 });
